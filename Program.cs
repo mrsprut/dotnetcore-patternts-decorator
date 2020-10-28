f930a34dca190011fcd3ed162b89402b8246b250
YuriiTrofimenko <tyaa@ukr.net>
2020-10-28 17:43:42 +0200

    demo

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using DecoratorSample.Beverage;
+using DecoratorSample.Decorators;
 
 namespace dotnet_scripts
 {
@@ -6,7 +8,29 @@ namespace dotnet_scripts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BeverageBase espresso = new Espresso();
+            BeverageBase blackTea = new BlackTea();
+            BeverageBase greenTea = new GreenTea();
+
+            PrintBeverage(espresso);
+            PrintBeverage(blackTea);
+            PrintBeverage(greenTea);
+
+            Console.WriteLine("-----------");
+
+            BeverageBase capuccino = new SugarCondiment(new MilkCondiment(new Espresso()));
+            PrintBeverage(capuccino);
+
+            BeverageBase greenTeaWithSugar = new SugarCondiment(new GreenTea());
+            PrintBeverage(greenTeaWithSugar);
+
+            Console.ReadLine();
+        }
+
+        static void PrintBeverage(BeverageBase beverage)
+        {
+            Console.WriteLine("Beverage: {0}; Price: {1}",
+                beverage.GetDescription(), beverage.GetCost());
         }
     }
 }
